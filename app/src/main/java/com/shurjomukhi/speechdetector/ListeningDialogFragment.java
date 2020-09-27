cd19f0683691a34c4d0fa3fa3b220b57a220357c
Monir <mknishad1@ymail.com>
2020-09-27 14:09:31 +0600

    set accuracy threshold to 50%

--- app/src/main/java/com/shurjomukhi/speechdetector/ListeningDialogFragment.java
+++ app/src/main/java/com/shurjomukhi/speechdetector/ListeningDialogFragment.java
@@ -136,7 +136,7 @@ public class ListeningDialogFragment extends DialogFragment {
   public void onSpeechEvent(SpeechEvent event) {
     //Toast.makeText(getActivity(), event.toString(), Toast.LENGTH_SHORT).show();
     Log.d(TAG, "onSpeechEvent: event = " + event);
-    if (event.getText().equalsIgnoreCase(getArguments().getString(ITEM_NAME)) && (event.getScore() * 100) >= 40.0f) {
+    if (event.getText().equalsIgnoreCase(getArguments().getString(ITEM_NAME)) && (event.getScore() * 100) >= 50.0f) {
       animationView.setAnimation(R.raw.trophy);
       animationView.setSpeed(2.0f);
       animationView.playAnimation();
